9345007e64723b3b4e7cd587ce17053de56f1268
Ruslan815 <ruslansaf2000@gmail.com>
2021-09-15 11:38:16 +0700

    Created beta version of BabyStepGiantStep method

--- Lab1/src/BabyStepGiantStep.java
+++ Lab1/src/BabyStepGiantStep.java
@@ -1,4 +1,6 @@
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class BabyStepGiantStep {
     /**
@@ -17,29 +19,51 @@ public class BabyStepGiantStep {
         in.close();
 
         int m = (int)Math.sqrt(p) + 1; // mk > p
-        int k = m; // m = k = sqrt(p) (+ 1) ?
+        int k = m; // Можно оставить только переменную М для оптимизации
         if (m * k <= p) {
             System.err.println("Need another mk!");
         }
 
-        // (a^i * y) mod p
-        int[] arrM = new int[m];
-        for (int i = 0; i < m; i++) {
-            arrM[i] = ((int)Math.pow(a, i) * y) % p;
+        // (a^j * y) mod p
+        // 0 <= j <= m - 1
+        int[] rowM = new int[m];
+        for (int j = 0; j < m; j++) {
+            rowM[j] = ((int)Math.pow(a, j) * y) % p;
         }
 
-        // a^j mod p
-        int[] arrK = new int[k];
+        // a^im mod p
+        // 1 <= i <= k
+        int[] rowK = new int[k];
         for (int i = 1; i <= k; i++) {
-            arrK[i - 1] = (int)Math.pow(a, i * m) % p;
+            rowK[i - 1] = (int)Math.pow(a, i * m) % p; // rowK is shifted 1 element to the left TODO use our method
         }
 
-        //TODO Сортируем массивы с сохранением исходных индексов
-        //TODO Находим одинаковые элементы
-        //TODO x = i * m - j
+        // Ищем одинаковые элементы
+        // a^im = a^j * y
+        int answer = -1;
+        Map<Integer, Integer> mapM = new TreeMap<>();
+        for (int j = 0; j < m; j++) {
+            mapM.put(rowM[j], j); // rowM starts with (a^0 * y), rowK starts with (a^1m)
+        }
+        // 0 <= j < m
+        // 1 <= i <= k
+        for (int i = 0; i < k; i++) {
+            if (mapM.containsKey(rowK[i])) {
+                answer = (i + 1) * m - mapM.get(rowK[i]); // x = i * m - j
+                System.out.println("j: " + mapM.get(rowK[i]) + ", i: " + (i + 1));
+                break;
+            }
+        }
+
+        //Answer: x = i * m - j
+        if (answer == -1) {
+            System.err.println("Cannot find a answer!");
+            return;
+        }
+        System.out.println("Answer: " + answer);
     }
 
     public static void main(String[] args) {
-
+        meth1();
     }
 }
