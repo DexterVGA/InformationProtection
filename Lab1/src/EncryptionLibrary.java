dbf27dc07e5e80d883ba48f951bb5c7ee3fcacce
Ruslan815 <ruslansaf2000@gmail.com>
2021-10-17 20:41:13 +0700

    Created template of class for Lab2.

---
+++ Lab1/src/EncryptionLibrary.java
@@ -0,0 +1,22 @@
+public class EncryptionLibrary {
+
+    public static void encryptionShamir() {
+
+    }
+
+    public static void encryptionElgamal() {
+
+    }
+
+    public static void encryptionRSA() {
+
+    }
+
+    public static void encryptionVernam() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
