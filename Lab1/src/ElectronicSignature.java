fa4cff05b0dbd1a10793f2ab90c77ebca61dbb27
Ruslan815 <ruslansaf2000@gmail.com>
2021-11-07 16:50:15 +0700

    Implemented Lab3.

--- Lab1/src/ElectronicSignature.java
+++ Lab1/src/ElectronicSignature.java
@@ -1,22 +1,14 @@
 import java.io.*;
-import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ElectronicSignature {
-    public static class RSAPair {
-        public byte[] h;
-        public long[] s;
-
-        public RSAPair(byte[] h, long[] s) {
-            this.h = h;
-            this.s = s;
-        }
-    }
-
     public static void RSA(byte[] message) {
         long P = ThreadLocalRandom.current().
                 nextLong(2 << 6, Integer.MAX_VALUE >> 17); // P is big prime number
@@ -89,26 +81,20 @@ public class ElectronicSignature {
     }
 
     public static byte[] fileToByteArray(String filename) {
-        List<Byte> resultList = new ArrayList<>();
-        try (FileInputStream fileInputStream = new FileInputStream(filename)) {
-            int currentByte;
-            while ((currentByte = fileInputStream.read()) != -1) {
-                resultList.add((byte) currentByte);
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ObjectOutputStream oos;
         try {
-            oos = new ObjectOutputStream(bos);
-            oos.writeObject(resultList);
+            return Files.readAllBytes(Path.of(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void writeByteArrayToFile(String filename, byte[] arr) {
+        try {
+            Files.write(Path.of(filename), arr);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return bos.toByteArray();
     }
 
     public static void Elgamal(byte[] message) {
@@ -134,7 +120,7 @@ public class ElectronicSignature {
         long k = ThreadLocalRandom.current().nextLong(1, P / 4);
         while (CryptographicLibrary.generalizedEuclidAlgorithm(++k, P - 1)[0] != 1);
         if (k >= P - 1) {
-            System.err.println("Не получилось сгенерировать число k.");
+            System.err.println("Elgamal: Не получилось сгенерировать число k.");
             return;
         }
         long r = CryptographicLibrary.fastExponentiationModulo(g, k, P);
@@ -172,23 +158,122 @@ public class ElectronicSignature {
         System.out.println("Elgamal: Valid signature.");
     }
 
-    public static void GOST() {
-        long Q = 1L << 16; // 16 bit
-        while (!CryptographicLibrary.isPrime(++Q)) ;
-
-        long P;
-        for (int i = 1; i <= 15; i++) {
-            long num = i * Q + 1;
-            if (CryptographicLibrary.isPrime(num) && num >= (1 << 30) && num < Integer.MAX_VALUE) {
-                P = num;
-                break;
+    public static void GOST(byte[] message) {
+        List<Long> listOfQ = new ArrayList<>();
+        for (int i = (1 << 15); i < (1 << 16); i++) {
+            if (CryptographicLibrary.isPrime(i)) {
+                listOfQ.add((long) i);
             }
         }
-        P = 1L << 30; // 31 bit
-        while (!CryptographicLibrary.isPrime(++P)) ;
+        long Q = listOfQ.get(ThreadLocalRandom.current().nextInt(0, listOfQ.size())); // 16 bit
+
+        List<Long> listOfP = new ArrayList<>();
+        for (int i = (1 << 15); i < (1 << 16); i++) {
+            long num = i * Q + 1;
+            if (CryptographicLibrary.isPrime(num) && num >= (1L << 30) && num < (1L << 31)) {
+                listOfP.add(num);
+            }
+        }
+        long P = listOfP.get(ThreadLocalRandom.current().nextInt(0, listOfP.size())); // 31 bit // P = bQ + 1
+
+        // System.out.println("Q & P: " + Q + " " + P);
+
+        long b = (P - 1) / Q;
+        long g = ThreadLocalRandom.current().nextLong(2, (P - 1) / 4);
+        while (CryptographicLibrary.fastExponentiationModulo(++g, b, P) <= 1) ;
+        long a = CryptographicLibrary.fastExponentiationModulo(g, b, P);
+        if (CryptographicLibrary.fastExponentiationModulo(a, Q, P) != 1) {
+            System.err.println("GOST: Не получилось сгенерировать параметр а!");
+            return;
+        }
+
+        // Общие параметры: p, q, a
+        // x - секретный ключ, у - открытый ключ
+
+        long x = ThreadLocalRandom.current().nextLong(1, Q);
+        long y = CryptographicLibrary.fastExponentiationModulo(a, x, P);
+
+        // Algorithm
+        final MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA3-256"); // returns 32 bytes array
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return;
+        }
+        byte[] hA = digest.digest(message);
 
 
+        // TODO Vadim
+        System.out.println(Arrays.toString(hA));
+        writeByteArrayToFile("R://Hello2.txt", hA);
+        hA = fileToByteArray("R://Hello2.txt");
+        System.out.println(Arrays.toString(hA));
+        // TODO Vadim
 
+
+        int [] intHA = byteArrToIntArr(hA);
+        long r;
+        long[] s = new long[intHA.length];
+        while (true) {
+            long k = ThreadLocalRandom.current().nextLong(1, Q);
+            r = CryptographicLibrary.fastExponentiationModulo(a, k, P) % Q;
+            if (r == 0) continue;
+            boolean isValidS = true;
+            for (int i = 0; i < s.length; i++) {
+                s[i] = (k * intHA[i] + x * r) % Q;
+                if (s[i] == 0) {
+                    isValidS = false;
+                    break;
+                }
+            }
+            if (!isValidS) continue;
+            break;
+        }
+
+        // message[0] = 1;
+        // r = 5;
+        // s[0] = 3;
+        // <m, r, s>
+
+        byte[] hB = digest.digest(message);
+        int [] intHB = byteArrToIntArr(hB);
+
+        if (r <= 0 || r >= Q) {
+            System.err.println("GOST: Invalid r!");
+            return;
+        }
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] <= 0 || s[i] >= Q) {
+                System.err.println("GOST: Invalid s!");
+                return;
+            }
+        }
+
+        int[] inverseH = new int[intHB.length];
+        for (int i = 0; i < inverseH.length; i++) {
+            inverseH[i] = (int) CryptographicLibrary.fastExponentiationModulo(intHB[i], -1, Q);
+        }
+
+        long[] u1 = new long[intHB.length];
+        long[] u2 = new long[intHB.length];
+        for (int i = 0; i < intHB.length; i++) {
+            u1[i] = (s[i] * inverseH[i]) % Q;
+            u2[i] = (-r * inverseH[i]) % Q;
+        }
+
+        long[] v = new long[intHB.length];
+        for (int i = 0; i < intHB.length; i++) {
+            v[i] = ((CryptographicLibrary.fastExponentiationModulo(a, u1[i], P) * CryptographicLibrary.fastExponentiationModulo(y, u2[i], P)) % P) % Q;
+        }
+
+        for (int i = 0; i < intHB.length; i++) {
+            if (v[i] != r) {
+                System.err.println("GOST: Invalid signature!");
+                return;
+            }
+        }
+        System.out.println("GOST: Valid signature.");
     }
 
     public static void main(String[] args) {
@@ -197,18 +282,6 @@ public class ElectronicSignature {
         byte[] fileInBytes = fileToByteArray(inFileName);
         ElectronicSignature.RSA(fileInBytes);
         Elgamal(fileInBytes);
-
-
-        /*StringBuilder stringBuilder = new StringBuilder();
-        for (char currentChar : passedMessage) {
-            stringBuilder.append(currentChar);
-        }
-
-        try (FileOutputStream fileOutputStream = new FileOutputStream(outFileName)) {
-            byte[] buffer = stringBuilder.toString().getBytes();
-            fileOutputStream.write(buffer, 0, buffer.length);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }*/
+        GOST(fileInBytes);
     }
 }
