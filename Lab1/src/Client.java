745ef2dca89eace89263e54bb97557d0b53047bd
Ruslan815 <ruslansaf2000@gmail.com>
2021-11-28 15:40:30 +0700

    Implemented Lab5.

---
+++ Lab1/src/Client.java
@@ -0,0 +1,81 @@
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Client {
+
+    private Server server;
+    private long N;
+    private long R;
+    public long NUMBER;
+
+    public Client(Server someServer) {
+        this.server = someServer;
+        this.N = server.N;
+    }
+
+    public long[] makeVote() {
+        // Task 1
+        int rnd = ThreadLocalRandom.current().nextInt(1, 1 << 15);
+        int v = ThreadLocalRandom.current().nextInt(0, 3); // 0 1 2
+        long n = rnd | ((long) v << 32); // first 32 bit = rnd; second 32 bit = v; v_rnd;
+        this.NUMBER = n;
+        //System.out.println(rnd);
+        System.out.println("Пользователь выбрал вариант: " + v);
+        //System.out.println(n);
+        System.out.println("Сформировали число n...");
+
+        // Task2
+        long r = ThreadLocalRandom.current().nextLong(11, Integer.MAX_VALUE >> 16);
+        while (CryptographicLibrary.generalizedEuclidAlgorithm(++r, N)[0] != 1) ;
+        this.R = r;
+
+        // Task 3
+        final MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA3-256"); // returns 32 bytes array
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+        byte[] h = digest.digest(longToBytes(n));
+        int[] intH = byteArrToIntArr(h); // 1 < h < N
+        System.out.println("Вычислили хэш-функцию от числа n...");
+
+        //Task 4
+        long[] inverseH = new long[intH.length];
+        for (int i = 0; i < inverseH.length; i++) {
+            inverseH[i] = (intH[i] * CryptographicLibrary.fastExponentiationModulo(r, server.D, N)) % N;
+        }
+        System.out.println("Передали по защищённому каналу от пользователя серверу хэш с чертой...");
+
+        return inverseH;
+    }
+
+    // Task 6
+    public long[] calculateSignature(long[] inverseS) {
+        long inverseR = CryptographicLibrary.fastExponentiationModulo(R, -1, N);
+        long[] s = new long[inverseS.length];
+        for (int i = 0; i < s.length; i++) {
+            s[i] = (inverseS[i] * inverseR) % N;
+        }
+        System.out.println("Пользователь вычислил подпись для своего бюллетеня...");
+        System.out.println("Пользователь отправил на сервер подписанный бюллетень <n, s> по анонимному каналу связи...");
+        return s;
+    }
+
+    public byte[] longToBytes(long x) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(x);
+        return buffer.array();
+    }
+
+    public static int[] byteArrToIntArr(byte[] bArr) {
+        int[] iArr = new int[bArr.length];
+        for (int i = 0; i < iArr.length; i++) {
+            iArr[i] = bArr[i] + 129;
+        }
+        return iArr;
+    }
+}
---
+++ Lab1/src/Server.java
@@ -0,0 +1,95 @@
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Server {
+    public long N;
+    public long D;
+    private long C;
+    private boolean isBlankPassed = false;
+
+    public void generateGeneralData() {
+        long P = ThreadLocalRandom.current().
+                nextLong(2 << 6, Integer.MAX_VALUE >> 17); // P is big prime number
+        while (!CryptographicLibrary.isPrime(++P)) ;
+        long Q = ThreadLocalRandom.current().
+                nextLong(2 << 6, Integer.MAX_VALUE >> 17); // Q is big prime number
+        while (!CryptographicLibrary.isPrime(++Q) || Q == P) ;
+
+        long N = P * Q;
+        long Fi = (P - 1) * (Q - 1);
+        long d = ThreadLocalRandom.current().nextLong(11, Integer.MAX_VALUE >> 16);
+        while (CryptographicLibrary.generalizedEuclidAlgorithm(++d, Fi)[0] != 1 && d < Fi) ;
+        if (d == Fi) {
+            System.err.println("Не получилось сгенерировать число d.");
+            return;
+        }
+        long c = CryptographicLibrary.generalizedEuclidAlgorithm(Fi, d)[2]; // Инверсия cd mod Fi = 1
+        if (c < 0) c += Fi;
+
+        this.N = N;
+        this.D = d;
+        this.C = c;
+    }
+
+    // Task 5
+    public long[] makeSignature(long[] inverseH) {
+        isBlankPassed = true;
+        long[] inverseS = new long[inverseH.length];
+        for (int i = 0; i < inverseS.length; i++) {
+            inverseS[i] = CryptographicLibrary.fastExponentiationModulo(inverseH[i], this.C, this.N);
+        }
+        System.out.println("Сервер помечает, что выдал бюллетень и вычисляет s с чертой и отправляет пользователю...");
+        return inverseS;
+    }
+
+    // Task 7
+    public void validateBlank(long n, long[] s) {
+        System.out.println("Сервер проверяет корректность бюллетеня...");
+        final MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA3-256"); // returns 32 bytes array
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return;
+        }
+        byte[] h = digest.digest(longToBytes(n));
+        int[] intH = byteArrToIntArr(h); // 1 < h < N
+        //intH[0] = 777;
+
+        for (int i = 0; i < intH.length; i++) {
+            long temp = CryptographicLibrary.fastExponentiationModulo(s[i], this.D, this.N);
+            if (intH[i] != temp) {
+                System.err.println("Некорректный бюллетень!");
+                return;
+            }
+        }
+        System.out.println("Корректный бюллетень.\nПроголосовали за вариант №" + (n >> 32));
+    }
+
+    public byte[] longToBytes(long x) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(x);
+        return buffer.array();
+    }
+
+    public static int[] byteArrToIntArr(byte[] bArr) {
+        int[] iArr = new int[bArr.length];
+        for (int i = 0; i < iArr.length; i++) {
+            iArr[i] = bArr[i] + 129;
+        }
+        return iArr;
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.generateGeneralData();
+
+        Client client = new Client(server);
+        long[] inverseH = client.makeVote();
+        long[] inverseS = server.makeSignature(inverseH);
+        long[] s = client.calculateSignature(inverseS);
+        server.validateBlank(client.NUMBER, s);
+    }
+}
